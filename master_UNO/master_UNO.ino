a9fa33c0e82ae2969470041642455cf25f543a50
Adnan Amin Siddiquee <38188511+adnan-22@users.noreply.github.com>
2018-12-26 15:05:24 +0600

    Add files via upload

---
+++ master_UNO/master_UNO.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+byte busStatus;
+float LMT;
+byte arrayLMT[4];
+void setup() {
+  Serial.begin(9600);
+  analogReference(INTERNAL);  //INTERNAK 1.1v for the Vref of ADC 
+  Wire.begin();                // join i2c bus
+  //checking the slave is present or not
+  do
+  {                                       
+    Wire.beginTransmission(0x08);
+    busStatus=Wire.endTransmission();
+  }while(busStatus!=0);
+}
+union 
+{
+  float  LMT;
+  byte arrayLMT[4];
+
+}myDATA;
+void loop() {
+  myDATA.LMT=100*(1.1/1024)*analogRead(A0); //calculating the temp using LM35 sensor;
+  Serial.println(myDATA.LMT);
+  //transmitting the digits to slave
+  Wire.beginTransmission(0x08); // transmit to slave (0x08)
+  Wire.write(myDATA.arrayLMT[3]);  
+  Wire.write(myDATA.arrayLMT[2]);  
+  Wire.write(myDATA.arrayLMT[1]);
+  Wire.write(myDATA.arrayLMT[0]);               
+  Wire.endTransmission();    // stop transmitting
+  delay(2000);
+}
---
+++ slave_NANO/slave_NANO.ino
@@ -0,0 +1,45 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);
+float LMT;
+byte arrayLMT[4];
+
+void setup() {
+  Serial.begin(9600);           
+  Wire.begin(0x08);               // join i2c bus with address 0x08
+  Wire.onReceive(receiveEvent);   // register event
+  //for I2C display
+  Wire.begin();
+  lcd.init();
+  lcd.backlight();
+}
+
+union
+{
+  byte arrayLMT[4];
+  float LMT;
+}myDATA;
+
+void loop() {
+  //printing on the Serial Monitor of Nano
+  Serial.print("LMT: ");
+  Serial.print(myDATA.LMT); 
+  Serial.println(" deg C");
+  
+  //printing on I2C display
+  lcd.setCursor(0,0);
+  lcd.print("LMT: ");
+  lcd.print(myDATA.LMT);
+  lcd.print(" ");
+  lcd.write(0xDF);//symbol of degree
+  lcd.print("C");
+  delay(2000);
+}
+
+void receiveEvent(int howMany) {
+  interrupts();
+  myDATA.arrayLMT[3]= Wire.read();
+  myDATA.arrayLMT[2]= Wire.read();
+  myDATA.arrayLMT[1]= Wire.read();
+  myDATA.arrayLMT[0]= Wire.read();
+}
